e033a09e39f8aace0c38ad58f4f5fe2d2f759441
Olivier Tavel <olivier.tavel@me.com>
2014-06-30 17:41:30 +0200

    ChatBox fonctionnelle

--- MultipeerConnection/MultipeerConnection/FirstViewController.h
+++ MultipeerConnection/MultipeerConnection/FirstViewController.h
@@ -8,6 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FirstViewController : UIViewController
+#import "AppDelegate.h"
+
+@interface FirstViewController : UIViewController <UITextFieldDelegate>
+
+@property (nonatomic, strong) AppDelegate *appDelegate;
+
+@property (weak, nonatomic) IBOutlet UITextField *txtMessage;
+@property (weak, nonatomic) IBOutlet UITextView *tvChat;
+
+- (void)sendMyMessage;
+- (void)didReceiveDataWithNotification:(NSNotification *)notification;
+
+- (IBAction)sendMessage:(id)sender;
+- (IBAction)cancelMessage:(id)sender;
 
 @end
--- MultipeerConnection/MultipeerConnection/FirstViewController.m
+++ MultipeerConnection/MultipeerConnection/FirstViewController.m
@@ -17,6 +17,15 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+	
+	self.appDelegate = (AppDelegate*)[[UIApplication sharedApplication] delegate];
+	
+	self.txtMessage.delegate = self;
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self
+											 selector:@selector(didReceiveDataWithNotification:) name:@"MCDidReceiveDataNotification"
+											   object:nil];
+	
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +35,46 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)sendMyMessage {
+	NSData *dataToSend = [self.txtMessage.text dataUsingEncoding:NSUTF8StringEncoding];
+	NSArray *allPeers = self.appDelegate.mcManager.session.connectedPeers;
+	NSError *error;
+	
+	[self.appDelegate.mcManager.session sendData:dataToSend
+										 toPeers:allPeers
+										withMode:MCSessionSendDataReliable
+										   error:&error];
+	
+	if (error) {
+		NSLog(@"%@", [error localizedDescription]);
+	}
+	
+	[self.tvChat setText:[self.tvChat.text stringByAppendingString:[NSString stringWithFormat:@"I wrote:\n%@\n\n", self.txtMessage.text]]];
+	[self.txtMessage setText:@""];
+	[self.txtMessage resignFirstResponder];
+}
+
+-(void)didReceiveDataWithNotification:(NSNotification *)notification {
+	MCPeerID *peerID = [[notification userInfo] objectForKey:@"peerID"];
+	NSString *peerDisplayName = peerID.displayName;
+	NSData *receivedData = [[notification userInfo] objectForKey:@"data"];
+	NSString *receivedText = [[NSString alloc] initWithData:receivedData encoding:NSUTF8StringEncoding];
+	
+	[self.tvChat performSelectorOnMainThread:@selector(setText:) withObject:[self.tvChat.text stringByAppendingString:[NSString stringWithFormat:@"%@ wrote:\n%@\n\n", peerDisplayName, receivedText]]  waitUntilDone:NO];
+}
+
+#pragma mark - UITextField Delegate
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+	[self sendMyMessage];
+	
+	return YES;
+}
+
+- (IBAction)sendMessage:(id)sender {
+	[self sendMyMessage];
+}
+
+- (IBAction)cancelMessage:(id)sender {
+	[self.txtMessage resignFirstResponder];
+}
 @end
